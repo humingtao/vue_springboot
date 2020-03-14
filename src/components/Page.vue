68de951079fbb1cdc9de3e06522815819d8fdbdd
humingtao <983086529@qq.com>
2020-03-14 17:47:04 +0800

    初始化项目，建立新的page页面

---
+++ src/components/Page.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h3>这是一个分页演示</h3>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Page from '../components/Page.vue'
 
 Vue.use(VueRouter)
 
@@ -17,11 +18,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/page',
+    name: 'Page',
+    component: Page
   }
 ]
 
-const router = new VueRouter({
-  routes
-})
+const router = new VueRouter({ routes })
 
 export default router
